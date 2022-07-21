d6c57a3f461566a3ed810897c64305b22a358a3f
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-22 00:19:07 +0530

    Intial commit and setting up firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false
+  firebase:{
+    apiKey: "AIzaSyBajV3-DEGiddJAQ8l1B_hly9RGnPdt3qY",
+    authDomain: "todolist-2eb3a.firebaseapp.com",
+    databaseURL: "https://todolist-2eb3a-default-rtdb.firebaseio.com",
+    projectId: "todolist-2eb3a",
+    storageBucket: "todolist-2eb3a.appspot.com",
+    messagingSenderId: "877039583421",
+    appId: "1:877039583421:web:bf266aca0da2c075a0b7d4",
+    measurementId: "G-MEYFE115LC"
+  }
 };
 
 /*
